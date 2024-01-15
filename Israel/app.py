9542a24dd7ab9bc230e2f0f1761e059e76487353
Israel Dellinger <israel.dellinger@gmail.com>
2024-01-15 10:16:53 -0800

    flask app is running now

--- Israel/app.py
+++ Israel/app.py
@@ -12,7 +12,7 @@ model_files = {
     "UK": "UKnukepredictmodel.sav"
 }
 
-loaded_models = {country: pickle.load(open(f"/workspaces/Israel-Flask-Project/Israel/{filename}", 'rb'))
+loaded_models = {country: pickle.load(open(f"/Users/israeldellinger/Programming/Israel-Flask-Project/Israel/{filename}", 'rb'))
                  for country, filename in model_files.items()}
 
 @app.route("/", methods = ["GET", "POST"])
@@ -25,3 +25,6 @@ def index():
         selected_model = loaded_models.get(country_select)  # Retrieve the corresponding model
 
     return render_template('index.html', selected_model=selected_model)
+
+if __name__ == "__main__":
+    app.run(debug=False)
