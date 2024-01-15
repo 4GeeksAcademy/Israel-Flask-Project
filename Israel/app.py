112394500f4f903b2689851316308d1e3c202f13
Israel Dellinger <israel.dellinger@gmail.com>
2024-01-15 14:45:42 -0800

    moved models into their own folder

--- Israel/app.py
+++ Israel/app.py
@@ -12,7 +12,7 @@ model_files = {
     "UK": "UKnukepredictmodel.sav"
 }
 
-loaded_models = {country: pickle.load(open(f"/Israel-Flask-Project-Test/Israel/{filename}", 'rb'))
+loaded_models = {country: pickle.load(open(f"/Israel/models/{filename}", 'rb'))
                  for country, filename in model_files.items()}
 
 @app.route("/", methods=["GET", "POST"])
